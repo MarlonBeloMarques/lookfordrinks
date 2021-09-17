1fbb4573bab592ea0e56cfab94fb0a4984235176
Marlon Marques <marlon.belohd@gmail.com>
2021-09-17 08:45:41 -0400

    feat(test/e2e): test login flow

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -7,9 +7,23 @@ describe('Login flow', () => {
     await device.reloadReactNative();
   });
 
+  test('must check if the fields are required', async () => {
+    await element(by.text('LETS GO')).tap();
+    await expect(element(by.text('LETS GO'))).toBeVisible();
+  });
+
+  test('should validate the fields', async () => {
+    await element(by.id('name_id')).typeText('Marlon');
+    await element(by.id('password_id')).typeText('123');
+
+    await element(by.text('LETS GO')).tap();
+    await expect(element(by.text('LETS GO'))).toBeVisible();
+  });
+
   test('should login successfully', async () => {
-    await element(by.id('name_id')).typeText('marlon.belohd@example.com');
+    await element(by.id('name_id')).typeText('Marlon');
     await element(by.id('password_id')).typeText('123456');
+
     await element(by.text('LETS GO')).tap();
     await expect(element(by.text('LETS GO'))).toNotExist();
   });
