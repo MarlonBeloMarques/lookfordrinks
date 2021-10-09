3532351cba04582f2cadf18bad6a44883240b90d
Marlon Marques <marlon.belohd@gmail.com>
2021-10-09 17:05:52 -0400

    feat(screens/home): get my position and add point in map

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,11 +1,32 @@
 import React, { FC } from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
 import { Block } from '~/components';
 
-const Home: FC = () => {
+type Props = {
+  position: Geolocation.GeoPosition;
+};
+
+const Home: FC<Props> = ({ position }) => {
   return (
     <Block>
-      <MapView testID="mapView" style={{ flex: 1 }}></MapView>
+      <MapView
+        testID="mapView"
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 1,
+          longitudeDelta: 1,
+        }}
+      >
+        {position && (
+          <Marker
+            coordinate={position.coords}
+            title="Your real-time location"
+          />
+        )}
+      </MapView>
     </Block>
   );
 };
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,66 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import { Alert } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 import Home from './Home';
+import { hasLocationPermission } from './permissions';
+
+const initialPositionValue: Geolocation.GeoPosition = {
+  coords: {
+    accuracy: 0,
+    altitude: 0,
+    latitude: 37.78825,
+    longitude: -122.4324,
+    heading: null,
+    speed: null,
+    altitudeAccuracy: null,
+  },
+  timestamp: 0,
+  mocked: true,
+  provider: 'passive',
+};
 
 const HomeContainer: FC = () => {
-  return <Home />;
+  const [myPosition, setMyPosition] =
+    useState<Geolocation.GeoPosition>(initialPositionValue);
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const getLocation = async () => {
+    try {
+      const hasPermission = await hasLocationPermission();
+
+      if (!hasPermission) {
+        return;
+      }
+
+      Geolocation.getCurrentPosition(
+        (position) => {
+          console.log(position);
+          setMyPosition(position);
+        },
+        (error) => {
+          Alert.alert(`Code ${error.code}`, error.message);
+          console.log(error);
+        },
+        {
+          accuracy: {
+            android: 'high',
+            ios: 'best',
+          },
+          enableHighAccuracy: true,
+          timeout: 60000,
+          maximumAge: 540000,
+          distanceFilter: 0,
+        },
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return <Home position={myPosition} />;
 };
 
 export default HomeContainer;
